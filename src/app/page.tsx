3d73576a160ee0237817366c558bc37fea6bcce4
Lucas TKP <lucasgeansantos@gmail.com>
2023-06-12 20:41:56 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import Form from '../components/Home/form'
-import Head from "next/head";
-import FaviIcon from '../../public/note-svgrepo-com.svg'
 
 function Page() {
   return (
     <section className='flex w-full justify-center mt-[30px] font-[600]'>
-      <Head>
-        <link rel="shortcut icon" href={FaviIcon} />
-      </Head>
+
         <div className='w-[90%] max-w-[500px]'>
             <p className='text-[40px] max-md:text-[30px]'>Criar nova nota</p>
             <Form />
